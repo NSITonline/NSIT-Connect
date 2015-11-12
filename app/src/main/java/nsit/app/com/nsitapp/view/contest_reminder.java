3f31e3b88c1f702f4326d5ed820f7190a4382621
SUMIT VOHRA <sumitvohra90@gmail.com>
2015-11-12 19:15:08 +0530

    sml chngs

--- app/src/main/java/nsit/app/com/nsitapp/view/contest_reminder.java
+++ app/src/main/java/nsit/app/com/nsitapp/view/contest_reminder.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 import android.transition.Fade;
 import android.transition.Slide;
 import android.util.Log;
@@ -18,17 +17,16 @@ import android.view.MenuItem;
 import android.view.Window;
 import android.widget.ImageView;
 
-import nsit.app.com.nsitapp.NotificationService;
 import nsit.app.com.nsitapp.R;
 import nsit.app.com.nsitapp.sync.SyncAdapter;
 
 
-public class contest_reminder extends AppCompatActivity implements ContestListFragment.Callback {
+public class contest_reminder extends FragmentActivity implements ContestListFragment.Callback{
 
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     protected void onCreate(Bundle savedInstanceState) {
-        getIntent();
+             getIntent();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             // inside your activity (if you did not enable transitions in your theme)
             getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
@@ -40,19 +38,20 @@ public class contest_reminder extends AppCompatActivity implements ContestListFr
 
         }
 
-        setTitle("Contest Reminder");
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_contest_reminder);
 
 
         if (savedInstanceState == null) {
             Fragment fragment = new ContestFragment();
-            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            FragmentManager fragmentManager= getSupportFragmentManager();
             fragmentManager.
                     beginTransaction().
-                    replace(R.id.content_frame, fragment).
+                    replace(R.id.content_frame,fragment).
                     commit();
-            Log.v("we will win 44", "we");
+            Log.v("we will win 44","we");
         }
 
         /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -61,33 +60,29 @@ public class contest_reminder extends AppCompatActivity implements ContestListFr
 
         SyncAdapter.initializeSyncAdapter(this);
 
-        Log.v("we will win 6", "we");
+        Log.v("we will win 6","we");
 
-        boolean isReminderOn = false;
-        NotificationService.startAlarm(this);
+
+      /*  boolean isReminderOn = PreferenceUtil.isReminderOn(this);
+
         if (!NotificationService.isAlarmOn(this) && isReminderOn) {
-
-        }
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
+            NotificationService.startAlarm(this);
+        }*/
     }
 
-    //restore menu main to its orignal empty and create a new menu called mnu mainer
-    @Override
+//restore menu main to its orignal empty and create a new menu called mnu mainer
+   /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu2, menu);
         return true;
-    }
+    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if(item.getItemId()==android.R.id.home)
-            finish();
         switch (item.getItemId()) {
-            case R.id.action_settings:
-                Intent i = new Intent(this, SettingsActivity.class);
+            case R.id.action_settings :
+                Intent i = new Intent(this,SettingsActivity.class);
                 startActivity(i);
                 return true;
         }
@@ -96,7 +91,7 @@ public class contest_reminder extends AppCompatActivity implements ContestListFr
     }
 
     @Override
-    public void onItemSelected(Uri contestUri, ImageView logo) {
+    public void onItemSelected(Uri contestUri,ImageView logo) {
         Intent intent = new Intent(this, ContestDetailActivity.class);
         intent.setData(contestUri);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -111,7 +106,7 @@ public class contest_reminder extends AppCompatActivity implements ContestListFr
     public void onFilterChanged() {
         Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.content_frame);
         if (fragment instanceof ContestFragment) {
-            ((ContestFragment) fragment).onFilterChanged();
+            ((ContestFragment)fragment).onFilterChanged();
         }
     }
 }

de5da664fde8d8c29a857f4d3642e36c8f6b8254
SUMIT VOHRA <sumitvohra90@gmail.com>
2015-11-14 18:53:36 +0530

    abt me and icon

--- app/src/main/java/nsit/app/com/nsitapp/AboutUs.java
+++ app/src/main/java/nsit/app/com/nsitapp/AboutUs.java
@@ -6,12 +6,10 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.widget.ImageView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
-
-import functions.ButtonAnimation;
 
 /**
  * Created by Swati garg on 21-06-2015.
@@ -25,35 +23,35 @@ public class AboutUs extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    ImageView s1, s2, s3, s4, s5, s6;
-    TextView con, rep;
     Activity activity;
-
     @Override
-    public void onAttach(Activity activity) {
+    public void onAttach(Activity activity)
+    {
         super.onAttach(activity);
         this.activity = activity;
 
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_aboutus, container, false);
+        ImageView s1,s2,s3,s4,s5,s6,s8,s9,s10;
+        TextView con;
+        TextView rep;
         s1 = (ImageView) rootView.findViewById(R.id.linked_sidharth);
         s2 = (ImageView) rootView.findViewById(R.id.linked_swati);
         s3 = (ImageView) rootView.findViewById(R.id.facebook_sidharth);
         s4 = (ImageView) rootView.findViewById(R.id.facebook_swati);
+        s8= (ImageView) rootView.findViewById(R.id.facebook_sumit);
+        s9= (ImageView) rootView.findViewById(R.id.linked_sumit);
         s5 = (ImageView) rootView.findViewById(R.id.facebook_chetan);
         s6 = (ImageView) rootView.findViewById(R.id.linked_chetan);
         rep = (TextView) rootView.findViewById(R.id.GoToRepo);
         con = (TextView) rootView.findViewById(R.id.cont);
-
-
         s1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ButtonAnimation btnAnimation = new ButtonAnimation();
-                btnAnimation.animateButton(view, getActivity());
+                btnAnimation.animateButton(view,getActivity());
                 Uri uri = Uri.parse("https://in.linkedin.com/in/sdpatro");
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
@@ -63,8 +61,28 @@ public class AboutUs extends Fragment {
             @Override
             public void onClick(View view) {
                 ButtonAnimation btnAnimation = new ButtonAnimation();
-                btnAnimation.animateButton(view, getActivity());
+                btnAnimation.animateButton(view,getActivity());
                 Uri uri = Uri.parse("https://in.linkedin.com/pub/swati-garg/b9/371/a79");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        s8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ButtonAnimation btnAnimation = new ButtonAnimation();
+                btnAnimation.animateButton(view,getActivity());
+                Uri uri = Uri.parse("https://www.facebook.com/sumit.vohra.353?ref=br_rs");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        s9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ButtonAnimation btnAnimation = new ButtonAnimation();
+                btnAnimation.animateButton(view,getActivity());
+                Uri uri = Uri.parse("https://in.linkedin.com/in/sumit-vohra-224484a0");
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }
@@ -73,27 +91,25 @@ public class AboutUs extends Fragment {
             @Override
             public void onClick(View view) {
                 ButtonAnimation btnAnimation = new ButtonAnimation();
-                btnAnimation.animateButton(view, getActivity());
+                btnAnimation.animateButton(view,getActivity());
                 Uri uri = Uri.parse("https://www.facebook.com/sidharth.patro.39?fref=ts");
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }
-        });
-        s4.setOnClickListener(new View.OnClickListener() {
+        });s4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ButtonAnimation btnAnimation = new ButtonAnimation();
-                btnAnimation.animateButton(view, getActivity());
+                btnAnimation.animateButton(view,getActivity());
                 Uri uri = Uri.parse("https://www.facebook.com/swati.garg.1690");
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }
-        });
-        s5.setOnClickListener(new View.OnClickListener() {
+        });s5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ButtonAnimation btnAnimation = new ButtonAnimation();
-                btnAnimation.animateButton(view, getActivity());
+                btnAnimation.animateButton(view,getActivity());
                 Uri uri = Uri.parse("https://www.facebook.com/chetan.shukla.5205?fref=ts");
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
@@ -103,7 +119,7 @@ public class AboutUs extends Fragment {
             @Override
             public void onClick(View view) {
                 ButtonAnimation btnAnimation = new ButtonAnimation();
-                btnAnimation.animateButton(view, getActivity());
+                btnAnimation.animateButton(view,getActivity());
                 Uri uri = Uri.parse("https://in.linkedin.com/pub/chetan-shukla/100/415/455");
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
@@ -115,7 +131,7 @@ public class AboutUs extends Fragment {
             @Override
             public void onClick(View view) {
                 ButtonAnimation btnAnimation = new ButtonAnimation();
-                btnAnimation.animateButton(view, getActivity());
+                btnAnimation.animateButton(view,getActivity());
                 Uri uri = Uri.parse("https://github.com/Swati4star/NSIT-App-v2");
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
@@ -125,7 +141,7 @@ public class AboutUs extends Fragment {
             @Override
             public void onClick(View view) {
                 ButtonAnimation btnAnimation = new ButtonAnimation();
-                btnAnimation.animateButton(view, getActivity());
+                btnAnimation.animateButton(view,getActivity());
                 Uri uri = Uri.parse("https://github.com/Swati4star/NSIT-App-v2/wiki");
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);

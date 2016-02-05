5a6288212bcca4fad2fe8165b2b1da7e38cf9f02
Naman Maheshwari <naman1901@gmail.com>
2016-02-05 23:29:53 +0530

    Up to date with master

--- app/src/main/java/nsit/app/com/nsitapp/JudgeGroup.java
+++ app/src/main/java/nsit/app/com/nsitapp/JudgeGroup.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package nsit.app.com.nsitapp;
 
 import java.util.ArrayList;
@@ -16,23 +15,4 @@ public class JudgeGroup {
         this.GroupType = GroupType;
         this.CodeForces_Properties = CodeForces_Properties;
     }
-=======
-package nsit.app.com.nsitapp;
-
-import java.util.ArrayList;
-
-/**
- * Created by hp1 on 11-09-2015.
- */
-public class JudgeGroup {
-    String GroupHeader;
-    String GroupType;
-    ArrayList<String> CodeForces_Properties;
-
-    JudgeGroup(String GroupHeader, String GroupType, ArrayList<String> CodeForces_Properties) {
-        this.GroupHeader = GroupHeader;
-        this.GroupType = GroupType;
-        this.CodeForces_Properties = CodeForces_Properties;
-    }
->>>>>>> refs/remotes/Swati4star/master
 }
